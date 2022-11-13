9b128db148c38c2b54ec65df47d51b8dce08f3f0
Marielle Fidelis <80335125+Miius@users.noreply.github.com>
2022-11-13 19:03:14 -0300

    Tela Final

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,14 +8,22 @@ using UnityEngine.UI;
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] private Button play;
+    [SerializeField] private Button back;
 
     private void Awake()
     {
-        play.onClick.AddListener(Game);
+        if(play)
+            play.onClick.AddListener(Game);
+        if(back)
+            back.onClick.AddListener(Initial);
     }
 
     public void Game()
     {
         SceneManager.LoadScene(1);
+    }
+    public void Initial()
+    {
+        SceneManager.LoadScene(0);
     }
 }
