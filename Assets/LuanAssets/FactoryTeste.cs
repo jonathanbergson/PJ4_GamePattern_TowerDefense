51c8d1cb90142ce6c8e3ca41f8aeb488a6aa0e82
Marielle Fidelis <80335125+Miius@users.noreply.github.com>
2022-11-13 19:44:34 -0300

    Spawn

--- Assets/LuanAssets/FactoryTeste.cs
+++ Assets/LuanAssets/FactoryTeste.cs
@@ -1,34 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class FactoryTeste : MonoBehaviour
 {
-    /*public Mesh[] meshs;
-    public Material[] materials;*/
-
     public GameObject[] enemys;
     public Transform[] spawners;
-    public int spawnindex, spawnCount;
+    int spawnCount;
+    private int spawnIndex = 0;
+    
 
-    bool canCall = true;
-    IEnumerator Spaw()
-    {    
+    private void Start()
+    {
+        spawnCount = 6;
+        StartCoroutine(Spawn());
+    }
+    IEnumerator Spawn()
+    {
         for (int i = 0; i < spawnCount; i++)
         {
-            Debug.Log("Corrotina");
-            Instantiate(Factory.CreateObject(enemys[spawnindex]), spawners[Random.Range(0, spawners.Length)]);
-            yield return new WaitForSeconds(1f);
+            Instantiate(Factory.CreateObject(enemys[spawnIndex]), spawners[Random.Range(0, spawners.Length)]);
+            yield return new WaitForSeconds(0.5f);
         }
-        canCall = true;
-    }
 
-    public void Update()
-    {       
-        if (Input.GetKeyDown(KeyCode.S) && canCall)
-        {
-            StartCoroutine(Spaw());
-            canCall = false;          
-        }       
+        spawnCount += 3;
+        yield return new WaitForSeconds(2f);
+
+        if (spawnCount >= 12) yield break;
+        StartCoroutine(Spawn());
     }
 }
